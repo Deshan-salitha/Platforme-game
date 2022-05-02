04cab933e352a2dc08028ba292d16ceb32f82f8b
Deshan-salitha <71576127+Deshan-salitha@users.noreply.github.com>
2022-05-02 14:03:02 +0530

    1st commit

---
+++ OverWorld/main.py
@@ -0,0 +1,33 @@
+import pygame
+import sys
+
+from settings import *
+from Overworld import Overworld
+
+
+class Game:
+    def __init__(self):
+        self.max_level = 2
+        self.overworld = Overworld(0, self.max_level, screen)
+
+    def run(self):
+        self.overworld.run()
+
+
+# Pygame setup
+pygame.init()
+screen = pygame.display.set_mode((screen_width, screen_height))
+clock = pygame.time.Clock()
+game = Game()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    screen.fill('black')
+    game.run()
+
+    pygame.display.update()
+    clock.tick(60)
